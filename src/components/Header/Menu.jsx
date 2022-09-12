c4eeb91f3c3f5a0fb657d0663eb7ac0aa444a061
swpw <pawel20702@gmail.com>
2022-09-13 01:58:30 +0200

    Menu toggle animate

--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -5,25 +5,31 @@ import css from './Menu.module.scss'
 import { gsap_openMenu, gsap_closeMenu } from './animations';
 import { getRoutes } from '../../hooks/getRoutes'
 
+import { gsap } from "gsap";
+
+
 
 export const Menu = ({ isMenuOpen }) => {
 	let menuRef = useRef(null)
+	let containerRef = useRef(null)
 
 	useEffect(() => {
-		isMenuOpen ? gsap_openMenu(menuRef) : gsap_closeMenu(menuRef)
+		isMenuOpen ? gsap_openMenu(menuRef, containerRef) : gsap_closeMenu(menuRef, containerRef)
 	}, [isMenuOpen])
 
 	return (
 		<nav className={css.nav} ref={el => menuRef = el}>
-			<ul>
-				{
-					getRoutes().map(({ path, Component: { name } }) => (
-						<li key={name}>
-							<Link to={path}>{name}</Link>
-						</li>
-					))
-				}
-			</ul>
+			<div className={css.navContainer} ref={el => containerRef = el}>
+				<ul>
+					{
+						getRoutes().map(({ path, Component: { name } }) => (
+							<li key={name}>
+								<Link to={path}>{name}</Link>
+							</li>
+						))
+					}
+				</ul>
+			</div>
 		</nav>
 	)
 }
--- src/components/Header/animations.js
+++ src/components/Header/animations.js
@@ -73,18 +73,33 @@ export const gsap_revealHeader = target => {
 	})
 }
 
-export const gsap_openMenu = target => {
-	gsap.to(target, {
-		autoAlpha: 1,
-		duration: .6,
-		ease: Power3.easeInOut
-	})
+export const gsap_openMenu = (menu, container) => {
+	gsap.timeline()
+		.to(['body', 'html'], {
+			css: { overflow: 'hidden' },
+			duration: 0,
+		})
+		.fromTo([menu, container], {
+			autoAlpha: 1,
+			clipPath: 'inset(0% 0% 0% 100%)',
+		}, {
+			clipPath: 'inset(0% 0% 0% 0%)',
+			duration: .75,
+			stagger: .15,
+			ease: Power3.easeInOut
+		})
 }
 
-export const gsap_closeMenu = target => {
-	gsap.to(target, {
-		autoAlpha: 0,
-		duration: .6,
-		ease: Power3.easeInOut
-	})
+export const gsap_closeMenu = (menu, container) => {
+	gsap.timeline()
+		.to([menu, container], {
+			clipPath: 'inset(0% 0% 0% 100%)',
+			duration: .75,
+			stagger: -.15,
+			ease: Power3.easeInOut
+		})
+		.to(['body', 'html'], {
+			css: { overflow: 'hidden' },
+			duration: 0,
+		})
 }
