85b1ac1a5cc17c4588f66c4afee344039769c789
swpw <pawel20702@gmail.com>
2022-11-01 21:56:38 +0100

    todo

--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -14,10 +14,90 @@ import { gsap } from "gsap";
 
 export const Menu = ({ headerRef, isMenuOpen }) => {
 	const [canvas, setCavnas] = useState(null)
+	const [isNewFirstImgEntry, setIsNewFirstImgEntry] = useState(null)
 
 	let menuRef = useRef(null)
 	let containerRef = useRef(null)
 	let canvasRef = useRef(null)
+	let imgListRef = useRef(null)
+	let contactWrapperRef = useRef(null)
+	let linksListRef = useRef(null)
+
+	const linksEnterHandler = (e) => {
+		console.log(e)
+		const current = e.currentTarget
+
+		const links = [...linksListRef.current.querySelectorAll('a')]
+		const excludedLinks = links
+			.map(el => el.getAttribute('data-index') !== current.getAttribute('data-index') && el)
+			.filter(el => el !== false)
+
+		// [ul children].indexOf(li)
+		const imgIndex = [...current.parentNode.parentNode.children].indexOf(current.parentNode)
+
+
+		/*
+		SPRAWDZIĆ CZY TO JEST PIERWSZE NAJECHANIE NA JAKIKOLWIEK LINK CZY
+		PRZECHODZI ON Z POPRZEDNIEGO LINKU:
+
+			JEŚLI JEST TO PIERWSZE TO:
+				WYŁĄCZYĆ:
+					wszystkie linki oprócz tego, który jest aktywny(mouseenter)
+					sekcja kontaktowa z lewego dolnego rogu
+					animacje tła w tle na ruszanie myszki
+				WŁĄCZYĆ:
+					animacje przejścia zdjęcia - type1 aka 'Clip path'
+				USTAWIĆ:
+					state isNewFirstImgEntry jako taki, który dostał już
+						pierwsze wejśćie, a każde inne będzie przenosiło do
+						stanu KOLEJNEGO PRZEJŚCIA (state = 1)
+
+			JEŚLI JEST TO KOLEJNE PRZEJŚCIE TO
+				WŁĄCZYĆ:
+					animacja przejścia między jednym a drugim zdjęciem - type2 aka 'Opacity / Blur'
+				
+				JEŚLI CZĘŚĆ KONTENTU ZOSTAŁA JUŻ WŁĄCZONA (timeout 1s state):
+					WYŁĄCZYĆ
+						wyłączyć ponownie kontent który został odkryty
+
+				ZMIENIĆ:
+					usunąć timeout z linksLeaveHandler
+		*/
+
+		// console.log(e, excludedLinks, imgIndex)
+	}
+
+	const linksLeaveHandler = e => {
+		/*
+		USTAWIĆ TIMEOUT NA DAJMY NA TO 2 SEKUNDY:
+			PO UPŁYWIE CZASU:
+				1 SEKUNDA:
+					WŁĄCZYĆ:
+						wszystkie linki oprócz tego, który jest aktywny(mouseenter) bo już jest aktywny duuh
+						sekcja kontaktowa z lewego dolnego rogu
+				2 SEKUNDY:
+					WŁĄCZYĆ:
+						animacje tła w tle na ruszanie myszki
+					WYŁĄCZYĆ:
+						aktywne zdjęcie - type1 ala 'Clip path
+		 */
+	}
+
+	const linksListLeaveHandler = e => {
+		/*
+		NATYCHMIASTOWO:
+			WŁĄCZYĆ:
+						wszystkie linki oprócz tego, który jest aktywny(mouseenter) bo już jest aktywny duuh
+						sekcja kontaktowa z lewego dolnego rogu
+						animacje tła w tle na ruszanie myszki
+			WYŁĄCZYĆ:
+				aktywne zdjęcie - type1 ala 'Clip path
+				timeout z linksLeaveHandler'a
+			USTAWIĆ:
+					state isNewFirstImgEntry jako taki, który może dostać
+						ponownie pierwsze wejście (state = 0)
+		*/
+	}
 
 	useEffect(() => {
 		isMenuOpen ? gsap_openMenu(menuRef, containerRef) : gsap_closeMenu(menuRef, containerRef)
@@ -31,20 +111,35 @@ export const Menu = ({ headerRef, isMenuOpen }) => {
 		arcBlur: 80
 	})), [])
 
+	useEffect(() => {
+		const links = [...linksListRef.current.querySelectorAll('a')]
+
+		links.forEach(el => {
+			el.addEventListener('mouseleave', linksLeaveHandler)
+			el.addEventListener('mouseenter', linksEnterHandler)
+		})
+
+		linksListRef.current.addEventListener('mouseleave', linksListLeaveHandler)
+	}, [])
+
 	return (
 		<nav className={css.nav} ref={el => menuRef = el}>
 			<div className={css.navContainer} ref={el => containerRef = el}>
 				<div className={css.navCanvasBg}>
 					<canvas className={css.navCanvas} ref={canvasRef}></canvas>
 				</div>
-				<ul className={css.imgList}>
-					<li className={css.imgItem}>
-						<img className={css.img} src="https://images.pexels.com/photos/10413262/pexels-photo-10413262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-					</li>
+				<ul className={css.imgList} ref={imgListRef}>
+					{
+						getRoutes().map(({ Component: { name }, img }) => (
+							<li className={css.imgItem} key={name}>
+								<img className={css.img} src={img} />
+							</li>
+						))
+					}
 				</ul>
 
 				<div className={css.navWrapper}>
-					<div className={css.contactWrapper}>
+					<div className={css.contactWrapper} ref={contactWrapperRef}>
 						<address className={css.physicalAddress}>
 							<ul>
 								<li>
@@ -72,7 +167,7 @@ export const Menu = ({ headerRef, isMenuOpen }) => {
 							</ul>
 						</address>
 					</div>
-					<ul className={css.linkList}>
+					<ul className={css.linkList} ref={linksListRef}>
 						{
 							getRoutes().map(({ path, Component: { name }, title }, index) => (
 								<li className={css.linkItem} key={name}>
--- src/hooks/getRoutes.js
+++ src/hooks/getRoutes.js
@@ -4,8 +4,8 @@ import { Portfolio } from '../pages/Portfolio'
 import { Contact } from '../pages/Contact'
 
 export const getRoutes = () => [
-	{ path: '/', Component: Home, title: 'Home', img: '' },
-	{ path: '/aboutus', Component: AboutUs, title: 'About us', img: '' },
-	{ path: '/portfolio', Component: Portfolio, title: 'Portfolio', img: '' },
-	{ path: '/contact', Component: Contact, title: 'Contact', img: '' },
+	{ path: '/', Component: Home, title: 'Home', img: 'https://images.pexels.com/photos/9829615/pexels-photo-9829615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+	{ path: '/aboutus', Component: AboutUs, title: 'About us', img: 'https://images.pexels.com/photos/8657665/pexels-photo-8657665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+	{ path: '/portfolio', Component: Portfolio, title: 'Portfolio', img: 'https://images.pexels.com/photos/11592813/pexels-photo-11592813.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+	{ path: '/contact', Component: Contact, title: 'Contact', img: 'https://images.pexels.com/photos/9888656/pexels-photo-9888656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
 ]
